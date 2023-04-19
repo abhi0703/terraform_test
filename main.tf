c792ec62c8405fa8c424ed3f4e73d318d0e864f1
abhi0703 <agowda077@gmail.com>
2023-04-19 19:34:58 +0530

    for lb

--- main.tf
+++ main.tf
@@ -8,21 +8,37 @@ terraform {
 }
 
 provider "aws" {
-  
+
 }
 
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.example.id]
+  subnets            = [aws_subnet.example.id]
 }
 
-resource "aws_vpc" "main" {
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+}
+
+resource "aws_subnet" "example" {
+  cidr_block = "10.0.0.0/24" 
+}
+
+resource "aws_lb_target_group" "example" {
+  name_prefix      = "example-tg"
+  port             = 80
+  protocol         = "HTTP"
+  target_type      = "instance"
+  vpc_id           = aws_vpc.example.id
+  health_check {
+    path = "/health"
+  }
+}
+
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_lb_target_group" "lambda-example" {
-  name        = "tf-example-lb-tg"
-  target_type = "lambda"
-}
