997174181ca460a216d36bcfa29311b0642fc765
abhi0703 <agowda077@gmail.com>
2023-04-19 19:56:03 +0530

    for lb

--- main.tf
+++ main.tf
@@ -11,35 +11,14 @@ provider "aws" {
 
 }
 
-resource "aws_lb" "example" {
-  name               = "example-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.example.id]
-  
+resource "aws_lb_target_group" "ip-example" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "lambda"
+  vpc_id      = aws_vpc.main.id
 }
 
-resource "aws_security_group" "example" {
-  name_prefix = "example"
-}
-/*
-
-resource "aws_subnet" "example" {
-  cidr_block = "10.0.0.0/24" 
-}
-*/
-resource "aws_lb_target_group" "example" {
-  name_prefix      = "demo"
-  port             = 80
-  protocol         = "HTTP"
-  target_type      = "instance"
-  vpc_id           = aws_vpc.example.id
-  health_check {
-    path = "/health"
-  }
-}
-
-resource "aws_vpc" "example" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
-
