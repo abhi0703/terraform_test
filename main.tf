fe441d3bd994ee06c36b93cd1f3c99b6e174dbe9
abhi0703 <agowda077@gmail.com>
2023-04-20 13:36:00 +0530

    lb updated

--- main.tf
+++ main.tf
@@ -17,6 +17,14 @@ resource "aws_lb_target_group" "ip-example" {
   protocol    = "HTTP"
   target_type = "lambda"
   vpc_id      = aws_vpc.main.id
+  health_check {
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 }
 
 resource "aws_vpc" "main" {
